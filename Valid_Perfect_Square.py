4a66c448550b64a5b244280281cb8c5d85d554ee
21A91A05B8 <97424421+21A91A05B8@users.noreply.github.com>
2022-05-01 14:14:28 +0530

    import math n=int(input()) while(n):      number = int(input())      root = math.sqrt(number)      if int(root + 0.5) ** 2 == number:          print("True")      else:          print("False")

---
+++ Valid_Perfect_Square.py
@@ -0,0 +1,9 @@
+import math
+n=int(input())
+while(n):
+     number = int(input())
+     root = math.sqrt(number)
+     if int(root + 0.5) ** 2 == number:
+         print("True")
+     else:
+         print("False")
