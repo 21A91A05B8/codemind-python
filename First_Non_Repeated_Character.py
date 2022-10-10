fcc094e79ea973a5ece5c391856ed92d5e915fe1
21A91A05B8 <97424421+21A91A05B8@users.noreply.github.com>
2022-10-10 21:36:52 +0530

    t=int(input()) for i in range(t):     n=int(input())     s=input()[:n]     s=s.lower()     for j in s:         if(s.count(j)==1):             print(j)             break     else:         print("-1")

---
+++ First_Non_Repeated_Character.py
@@ -0,0 +1,11 @@
+t=int(input())
+for i in range(t):
+    n=int(input())
+    s=input()[:n]
+    s=s.lower()
+    for j in s:
+        if(s.count(j)==1):
+            print(j)
+            break
+    else:
+        print("-1")
