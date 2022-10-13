f7cc1e8b3ea022eb86f989a0cef8bd230956c795
21A91A05B8 <97424421+21A91A05B8@users.noreply.github.com>
2022-10-14 00:02:39 +0530

    s=str(input()) c=0 a=0 for i in range (0,len(s)):     c+=1     if(s[i]==' '):         print(c-1,end=' ')         c=0     elif(i==len(s)-1):         print(c,end=' ')

--- characters_in_each_word.py
+++ characters_in_each_word.py
@@ -1,4 +1,10 @@
-s=input().split()
-for i in s:
-    print(len(i),end=' ')
-    
+s=str(input())
+c=0
+a=0
+for i in range (0,len(s)):
+    c+=1
+    if(s[i]==' '):
+        print(c-1,end=' ')
+        c=0
+    elif(i==len(s)-1):
+        print(c,end=' ')
